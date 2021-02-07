82eee51efa773a3ace1c308fc72fb9f6bad6d8fd
cyhan <38914065+cyeehan@users.noreply.github.com>
2021-02-08 00:55:43 +0800

    docs: update resume

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -67,7 +67,7 @@ export default () => {
       <Seo title="YeeHan Chung's Resume" description={"After completing all my school assignments in my last semester and at least 10 days prior final examination, I decided to take a small break and embarked to build my personal website using Gatsby, Google Analytics, and other fun stuff. At the same time, uploaded my digital resume here."}/>
       <Post>
         <P style={{fontSize: "1.5rem", marginBottom: "1.5rem", fontWeight: "bold",}}>Resume</P>
-        <P size="xSmall" color="dark2"><Span>{"7 February, 2021"}</Span></P>
+        {/* <P size="xSmall" color="dark2"><Span>{"7 February, 2021"}</Span></P> */}
         <FooterSocialIcons>
           <InnerIcon className="social-icon" href="https://github.com/cyeehan" target="_blank" rel="noopener noreferrer">
             <img src={data.github.publicURL} alt="GitHub" />
@@ -82,20 +82,25 @@ export default () => {
             <img src={data.instagram.publicURL} alt="Instagram" />
           </InnerIcon>
         </FooterSocialIcons>
+        <br />
         {/* Professional Experience */}
         <h2>Professional Experience</h2>
         <h3>Software Engineer Intern @ Fusionex International</h3>
         <h4>Jan 2021 - Present | Petaling Jaya, Malaysia</h4>
         <li>Software and web application developments in the core team.</li>
-        <li>Support SIT (System Integration Testing) and UAT (User Acceptance Testing).</li>
-        <li>API integration, technical/non-technical documentation and system optimization.</li>
+        <li>Integrated API, and written clean and reusable codes.</li>
+        <li>Supported system integration testing and user acceptance testing.</li>
+        <li>Assisted team members to solve challenging coding problems.</li>
+        <li>Written detailed technical/non-technical documentation using Markdown for core team members.</li>
         <li>Tech stack: WordPress (PHP, JavaScript, jQuery/Ajax), Express.js (Node.js), REST API.</li>
+        <br /><br /><br />
         {/* Education */}
         <h2>Education</h2>
         <h3>INTI International University + Coventry University, UK</h3>
-        <h4>Apr 2018 - May 2021 | Malaysia</h4>
+        <h4>Apr 2018 - May 2021 | Negeri Sembilan, Nilai, Malaysia</h4>
         <li>Bachelor of Computer Science, majoring in Software Engineering.</li>
         <li>CGPA: 3.85/4.00 (First Class Honours).</li>
+        <br /><br /><br />
         {/* Projects */}
         <h2>Projects</h2>
         {/* 1st project */}
@@ -117,42 +122,54 @@ export default () => {
         {/* 2nd project */}
         <h3>Covid-19 Medicine + QR Code Scanner Android Apps</h3>
         <h4>Team-based Project</h4>
-        <li>Responsible for leading a team of 3, architecting solutions, developing, documenting, and pitching final products.</li>
+        <li>Led a team of 3, architected solutions, developed, documented, and pitched final products.</li>
         <li>Tech stack: Java, XML, Firebase, Covid-19 API, GitHub, Android Studio, Microsoft Forms, Power Automate, Power BI.</li>
         <li>GitHub repo: (1){" "}
           <a href="https://github.com/cyeehan/medicine-covid-19-health-check" rel="noopener noreferrer" target="_blank">Medicine app</a>
           , (2){" "}
           <a href="https://github.com/cyeehan/medicine-qr-code-scanner" rel="noopener noreferrer" target="_blank">QR Code Scanner app</a>
         </li>
-        <br />
+        <br /><br /><br />
         {/* Publication */}
         <h2>Publication</h2>
-        <h4>A Study of Software Quality Assurance Perspective on E-commerce System Development</h4>
-        <li>Main author. Paper is under reviewed by a publisher from USA.</li>
+        <h3>A Study of Software Quality Assurance Perspective on E-commerce System Development</h3>
+        <li>Main author. Paper is under review by a publisher from USA.</li>
         <li>Expected to be published in SCOPUS indexed journal in 2021.</li>
+        <br /><br /><br />
         {/* Leadership experience */}
         <h2>Leadership Experience</h2>
         <h3>Modern Workplace Ambassador @ Microsoft Malaysia</h3>
         <h4>Apr 2019 - Present | Kuala Lumpur, Malaysia</h4>
         <li>Worked closely with Adoption Specialist and Operation Teams to drive Microsoft Teams consumption.</li>
-        <li>Led and organized various impactful events as one of the two Squad Leaders for 3 months.</li>
-        <li>Increased over 700 Microsoft Teams account activation, and Microsoft Malaysia winning an award for the event.</li>
-        <li>Organized 3-day Virtual Bootcamp & Recruitment Sessions for over 170 students and received 4.68/5 avg. rating.</li>
+        <li>Led and organized various impactful events as one of the two squad leaders for 3 months.</li>
+        <li>Managed projects using GitHub, Trello with Kanban board, and Microsoft Teams Planner.</li>
+        <li>Increased over 700 Microsoft Teams account activation, and helped Microsoft Malaysia won an award.</li>
+        <li>Organized 3-day Virtual Training & Recruitment Sessions for 170+ students and received 4.68/5 avg. rating.</li>
+        <li>Provided Microsoft Office 365 training to 150+ students across campuses (virtually and physically).</li>
         <br />
         <h3>Student Leader Ambassador @ INTI International University</h3>
-        <h4>Jul 2018 - Present | Negeri Sembilan, Nilai</h4>
-        <li>Interviewed 30+ undergraduate and graduate students during recruitment of new Leader Ambassador.</li>
+        <h4>Jul 2018 - Present | Negeri Sembilan, Nilai, Malaysia</h4>
+        <li>Interviewed 30+ undergraduate and graduate students during recruitment of new Leader Ambassadors.</li>
         <li>Moderated Virtual Hiring Session with Exabytes Group and Dim Tai Fung in Virtual Career Fairs 2020.</li>
-        <li>Led a team of 13 to provide résumé writing tips & tricks to over 80 INTI students to increase employability.</li>
-        <li>Professionally produced 2 Career Fair and 5 interview videos using Adobe Premiere Pro. Designed 3 posters using Photoshop.</li>
+        <li>Led a team of 13 to provide résumé writing tips & tricks to 80+ INTI students to increase employability.</li>
+        <li>Professionally produced two{" "}
+          <a href="https://www.facebook.com/intiiu.cs/videos" rel="noopener noreferrer" target="_blank">Career Fair videos</a> 
+          {" "}and five{" "}
+          <a href="https://www.facebook.com/intiiu.cs/videos" rel="noopener noreferrer" target="_blank">interview videos</a> 
+          {" "}using Adobe Premiere Pro.
+        </li>
+        <li>Professionally supported in designing three{" "}
+          <a href="https://www.facebook.com/intiiu.cs/photos" rel="noopener noreferrer" target="_blank">Career Fair posters</a> 
+          {" "}using Photoshop.
+        </li>
         <li>Facilitated 5 Industry Advisory Board events and INTI’s Partnership Appreciation Dinner.</li>
         <br />
         <h3>Program Master @ INTI International University IT Club</h3>
-        <h4>Jan 2019 - Nov 2019 | Negeri Sembilan, Nilai</h4>
-        <li>Coordinator of Microsoft Office 365 Training with over 60 INTI students.
-        </li>
+        <h4>Jan 2019 - Nov 2019 | Negeri Sembilan, Nilai, Malaysia</h4>
+        <li>Coordinator of Microsoft Office 365 Training with over 60 INTI students.</li>
         <li>Chair-organized Python Programming Workshop for 20 INTI students.</li>
         <li>Program Master of Arduino Programming Workshop for 80 INTI students.</li>
+        <br /><br /><br />
         {/* Awards and Achievements */}
         <h2>Awards and Achievements</h2>
         <table>
@@ -167,7 +184,7 @@ export default () => {
               <li>Digitised at least 20 physical registration forms.</li>
               <li>Developed a login system and an administrative dashboard with CRUD operations.</li>
               <li>Pitched final system to Coca-Cola Malaysia Managing Director and employers.</li>
-              <li>Technologies: HTML, CSS, SQL, PHP, Bootstrap, phpMyAdmin, PhpStorm.</li>
+              <li>Tech stack: HTML, CSS, SQL, PHP, Bootstrap, phpMyAdmin, PhpStorm.</li>
               <img className="resume-img" src={cocaCola} alt="Girl in a jacket"/>
             </td>
           </tr>
