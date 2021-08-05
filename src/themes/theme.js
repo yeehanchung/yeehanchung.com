d8c8693b7c20bbe8a682d37a5458502ea34a5e88
Yee Han <cyh0011215@gmail.com>
2021-08-06 01:10:20 +0800

    refactor: format codes

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,49 +1,46 @@
 /**
  * Hydrate styling when page is first loaded
  */
-import '../styles/main.module.css'
 
 export default {
   fonts: {
-    // Go to postElement.js
+    // Used in styled/Post.js
     main:
-      "system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif,BlinkMacSystemFont,Helvetica,Arial,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol",
-    // font-family: medium-content-sans-serif-font, "Lucida Grande", "Lucida Sans Unicode", "Lucida
-    code: "Roboto Mono, monospace",
+      'system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif,BlinkMacSystemFont,Helvetica,Arial,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol',
+    code: 'Roboto Mono, monospace',
   },
   size: {
     xxSmall: '0.25rem',
   },
   colors: {
-    // main1: "hsl(207, 70%, 59%)",
-    main1: "#357ae8",
-    main2: "hsl(207, 70%, 94%)",
-    dark1: "hsl(227, 2%, 12%)",
-    dark2: "hsl(227, 2%, 26%)",
-    dark3: "hsl(227, 2%, 64%)",
-    dark4: "#414244",
-    light1: "hsl(0, 0%, 100%)",
-    light2: "hsl(0, 0%, 95%)",
+    main1: '#357ae8',
+    main2: 'hsl(207, 70%, 94%)',
+    dark1: 'hsl(227, 2%, 12%)',
+    dark2: 'hsl(227, 2%, 26%)',
+    dark3: 'hsl(227, 2%, 64%)',
+    dark4: '#414244',
+    light1: 'hsl(0, 0%, 100%)',
+    light2: 'hsl(0, 0%, 95%)',
   },
   breakpoints: {
-    mobile: "only screen and (max-width: 50rem)",
-    tablet: "only screen and (max-width: 65rem)",
+    mobile: 'only screen and (max-width: 50rem)',
+    tablet: 'only screen and (max-width: 65rem)',
   },
   spacings: {
-    xxSmall: ".25rem",
-    xSmall: ".5rem",
-    small: "0rem",
-    medium: "2rem",
-    large: "8rem",
-    xLarge: "3rem",
-    x2Large: "3.5rem",
-    xxLarge: "3rem",
+    xxSmall: '.25rem',
+    xSmall: '.5rem',
+    small: '0rem',
+    medium: '2rem',
+    large: '8rem',
+    xLarge: '3rem',
+    x2Large: '3.5rem',
+    xxLarge: '3rem',
   },
   animations: {
-    button: "box-shadow 0.3s ease",
-    link: "color 0.2 ease",
+    button: 'box-shadow 0.3s ease',
+    link: 'color 0.2 ease',
   },
   shadows: {
-    shadow1: "2px 5px 20px rgba(30, 30, 31, 0.05)",
+    shadow1: '2px 5px 20px rgba(30, 30, 31, 0.05)',
   },
-}
+};
