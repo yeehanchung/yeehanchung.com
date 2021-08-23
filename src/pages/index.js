86a36be0df9482f8f4ee9f2cd8d4aec8b6ab460b
Yee Han <cyh0011215@gmail.com>
2021-08-23 23:04:41 +0800

    feat: add, update. and remove files inside pages dir

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,101 @@
-import { graphql } from 'gatsby';
-import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
-import { Container, Post, Seo } from '../components';
+import styled from 'styled-components';
+import { Container, Seo } from '../components';
 import { Author } from '../components/Author.js';
-import * as Typography from '../styled/Typography';
 import OG_IMAGE from '../img/og_image.jpeg';
+import './blog.css';
 
-export const mainPageQuery = graphql`
-  query ResumeQuery {
-    mdx(frontmatter: { tag: { eq: "resume" } }) {
-      body
-      frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        title
-        slug
-        author
-        description
-        keywords
-      }
-      # fields {
-      #   readingTime {
-      #     text
-      #   }
-      # }
-    }
+export const HomeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: ${(props) => props.theme.fonts.main};
+  padding: 3rem 0;
+  max-width: 90ch;
+  padding: 2rem 0rem 3rem;
+  margin: 0 auto 2rem;
+  align-items: center;
+
+  h3 {
+    font-size: 1rem;
+    font-weight: bold;
+    margin: 2rem 0 1rem 0;
+    /* line-height: 1.5rem; */
+    font-size: 1.1rem;
+    font-weight: bold;
+    margin: 0.8rem 0 0.25rem 0;
+  }
+
+  p {
+    font-size: 0.9rem;
+    line-height: 1.5rem;
+    font-weight: 400;
+    color: hsl(227, 2%, 26%);
+    display: -webkit-inline-box;
+    display: -webkit-inline-flex;
+    display: -ms-inline-flexbox;
+    display: inline-flex;
+    margin-bottom: 1.25rem;
+    margin-bottom: 0.5rem;
   }
 `;
 
-export default ({ data }) => {
-  let metadata = data.mdx.frontmatter;
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      markdownRemark(frontmatter: { tag: { eq: "resume" } }) {
+        html
+        fields {
+          readingTime {
+            text
+          }
+        }
+        frontmatter {
+          author
+          date(formatString: "MMM DD, YYYY")
+          description
+          keywords
+          slug
+          excerpt
+          tag
+          title
+          lastEdited(formatString: "MMMM DD, YYYY")
+        }
+      }
+    }
+  `);
+  let queryData = data.markdownRemark.frontmatter;
+  // let gitSVG = data.git.publicURL
+  // console.log(queryData.keywords);
+  // console.log(typeof newData)
 
   return (
+    // <NoteWrapper>
     <Container>
       <Seo
         image={OG_IMAGE}
-        title={metadata.title}
-        author={metadata.author}
-        keywords={metadata.keywords}
+        title={queryData.title}
+        author={queryData.author}
+        keywords={queryData.keywords}
       />
-      <Post>
-        <blockquote>
+      <HomeWrapper>
+        {/* <blockquote>
           <p>
             Nothing lights up Advocates like creating a solution that changes
             people’s lives.
           </p>
-        </blockquote>
-        <Author />
-        <Typography.H2>{data.mdx.frontmatter.description}</Typography.H2>
+        </blockquote> */}
+        <Author ogImage={OG_IMAGE} />
+      </HomeWrapper>
+      {/* <Typography.H2>{queryData.description}</Typography.H2>
         <p style={{ marginBottom: '0.8rem' }}>
-          <span>⏳</span>
-          {data.mdx.frontmatter.date}
-        </p>
-        <MDXRenderer>{data.mdx.body}</MDXRenderer>
-      </Post>
+          <span role="img">⏳</span> {queryData.date}
+        </p> */}
+      {/* <MDXRenderer>{data.mdx.body}</MDXRenderer> */}
     </Container>
   );
 };
+
+// 55549520-64fd-5e35-af32-edc323fc88ec
+// b3e047f9-306c-5f21-a77b-f03e1ab7f0b4
+// resume: 9aa40867-12c2-5628-921d-2eaf8e3caa6e
