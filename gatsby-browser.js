fa0d5d35d294e37aa19bafc030f29820e86591d9
Yee Han <cyh0011215@gmail.com>
2021-08-23 22:53:58 +0800

    refactor: update gatsby files

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,23 +7,46 @@
 // You can delete this file if you're not using it
 
 import React from 'react';
+// { GlobalStyles }
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
+// import './language-tabs.css';
+import './src/styles/global.css';
+import './src/styles/main.module.css';
+// Prism
+import { PrismStyles } from './src/styles/PrismStyles';
 import Theme from './src/themes/theme';
-import './src/styles/main.module.scss';
 
-// const GlobalStyles = createGlobalStyle`
-//   * {
-//     box-sizing: border-box;
-//     margin: 0;
-//     padding: 0;
-//   }
+const GlobalStyles = createGlobalStyle`
+  /* * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  } */
 
-//   body {
-//   }
-// `;
+  ${PrismStyles}
+`;
+
+// const components = {
+//   // table: Table,
+//   pre: (preProps) => {
+//     const props = preToCodeBlock(preProps);
+//     // if there's a codeString and some props, we passed the test
+//     if (props) {
+//       return <Code {...props} />;
+//     }
+//     return <pre {...preProps} />;
+//   },
+//   wrapper: ({ children }) => <>{children}</>,
+//   VisitorBadge: ({ src, alt }) => <VisitorBadge src={src} alt={alt} />,
+// };
 
 export const wrapRootElement = ({ element }) => (
-  <ThemeProvider theme={Theme}>{element}</ThemeProvider>
+  // <MDXProvider components={components}>
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+    {element}
+  </ThemeProvider>
+  // </MDXProvider>
 );
 
 export const wrapPageElement = ({ element }) => (
