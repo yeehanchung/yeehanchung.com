a4f646cf59f38345c360d3de25afec9fc20da61c
Yee Han <cyh0011215@gmail.com>
2021-12-16 23:46:14 +0800

    refactor(singlePost): create a styled div

--- src/templates/singlePost.js
+++ src/templates/singlePost.js
@@ -1,111 +1,113 @@
 /* --- LIBRARIES --- */
-import {graphql, Link} from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import React from 'react';
 import styled from 'styled-components';
 
 /* --- UI COMPONENTS --- */
-import {Container, Post, Seo} from '../components';
+import { Container, Post, Seo } from '../components';
 
 const Span = styled.span`
-    padding: 0 1rem 0 0rem;
+	padding: 0 1rem 0 0rem;
 `;
 
 const Tag = styled.span`
-    color: #ff5e62;
-    padding: 0 1rem 0 0rem;
-    font-weight: 700;
+	color: #ff5e62;
+	padding: 0 1rem 0 0rem;
+	font-weight: 700;
 `;
 
 const StyledPostContent = styled.div`
-    code {
-        background-color: var(--lightest-navy);
-        color: var(--lightest-slate);
-        padding: 0.2em 0.4em;
-    }
+	code {
+		background-color: var(--lightest-navy);
+		color: var(--lightest-slate);
+		padding: 0.2em 0.4em;
+	}
 
-    pre code {
-        background-color: transparent;
-        padding: 0;
-    }
+	pre code {
+		background-color: transparent;
+		padding: 0;
+	}
 `;
 
 const BackToNotes = styled(Link)`
-    color: ${(props) => props.theme.colors.link};
-    max-width: 90ch;
-    margin: 3.5rem auto 2rem;
-    display: block;
-    text-decoration: none;
+	color: ${(props) => props.theme.colors.link};
+	max-width: 90ch;
+	margin: 3.5rem auto 2rem;
+	display: block;
+	text-decoration: none;
 `;
 
 const LastEdited = styled.div`
-    padding-top: 35px;
+	padding-top: 35px;
 `;
 
-const singlePost = ({data}) => {
-    const readingTime = data.markdownRemark.fields.readingTime.text;
-    const excerpt = data.markdownRemark.frontmatter.excerpt;
-    const tag = data.markdownRemark.frontmatter.tag;
-    const lastEdited = data.markdownRemark.frontmatter.lastEdited;
+const Contents = styled.div`
+	margin-top: 50px;
+`;
 
-    return (
-        <Container>
-            <Seo
-                title={data.markdownRemark.frontmatter.title}
-                description={data.markdownRemark.frontmatter.excerpt}
-            />
-            <Post>
-                <h1>{data.markdownRemark.frontmatter.title}</h1>
-                <p style={{marginBottom: '50px'}}>
-                    <Span>{data.markdownRemark.frontmatter.date}</Span>
-                    <Span>{readingTime}</Span>
-                    <Span>{excerpt}</Span>
-                    <Tag>
-                        {'#'}
-                        {tag}
-                    </Tag>
-                </p>
-                <StyledPostContent
-                    dangerouslySetInnerHTML={{
-                        __html: data.markdownRemark.html,
-                    }}
-                />
-                {lastEdited ? (
-                    <LastEdited>
-                        <p>
-                            Last edited: {lastEdited}
-                        </p>
-                    </LastEdited>
-                ) : (
-                    ''
-                )}
-                <BackToNotes to={'/tech'}>← Back to notes</BackToNotes>
-            </Post>
-        </Container>
-    );
+const singlePost = ({ data }) => {
+	const readingTime = data.markdownRemark.fields.readingTime.text;
+	const excerpt = data.markdownRemark.frontmatter.excerpt;
+	const tag = data.markdownRemark.frontmatter.tag;
+	const lastEdited = data.markdownRemark.frontmatter.lastEdited;
+
+	return (
+		<Container>
+			<Seo
+				title={data.markdownRemark.frontmatter.title}
+				description={data.markdownRemark.frontmatter.excerpt}
+			/>
+			<Post>
+				<h1>{data.markdownRemark.frontmatter.title}</h1>
+				<Span>{data.markdownRemark.frontmatter.date}</Span>
+				<Span>{readingTime}</Span>
+				<Span>{excerpt}</Span>
+				<Tag>
+					{'#'}
+					{tag}
+				</Tag>
+				<Contents>
+					<StyledPostContent
+						dangerouslySetInnerHTML={{
+							__html: data.markdownRemark.html,
+						}}
+					/>
+				</Contents>
+				{lastEdited ? (
+					<LastEdited>
+						<p>Last edited: {lastEdited}</p>
+					</LastEdited>
+				) : (
+					''
+				)}
+				<BackToNotes to={'/tech'}>← Back to notes</BackToNotes>
+			</Post>
+		</Container>
+	);
 };
 
 export default singlePost;
 
 export const pageQuery = graphql`
-    query SinglePostQuery($id: String!) {
-        markdownRemark(id: {eq: $id}) {
-            fields {
-                readingTime {
-                    text
-                }
-            }
-            frontmatter {
-                # author
-                date(formatString: "MMM DD, YYYY")
-                # description
-                slug
-                tag
-                title
-                keywords
-                excerpt
-                lastEdited(formatString: "MMM DD, YYYY")
-            }
-            html
-        }
-    }
+	query SinglePostQuery($id: String!) {
+		markdownRemark(id: { eq: $id }) {
+			fields {
+				readingTime {
+					text
+				}
+			}
+			frontmatter {
+				# author
+				date(formatString: "MMM DD, YYYY")
+				# description
+				slug
+				tag
+				title
+				keywords
+				excerpt
+				lastEdited(formatString: "MMM DD, YYYY")
+			}
+			html
+		}
+	}
 `;
