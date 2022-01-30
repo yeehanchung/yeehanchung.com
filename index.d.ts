39d5ab5572654a47b9d54810bbdb947c8ad20c3a
Yee Han <cyh0011215@gmail.com>
2022-01-30 19:08:38 +0800

    (chore): ts type info

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare module "*.jpg"
+declare module "*.jpeg"
+declare module "*.css"
+declare module "*.scss"
+
+// export type ThemedStyledProps<P, T> = P & ThemeProps<T>;
+// export type StyledProps<P> = ThemedStyledProps<P, AnyIfEmpty<DefaultTheme>>;
+// export type IntrinsicElementsKeys = keyof JSX.IntrinsicElements;
