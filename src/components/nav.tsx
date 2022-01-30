2361bbd6ef25d03cfeced914fc37eb80cc3da115
Yee Han <cyh0011215@gmail.com>
2022-01-30 22:54:46 +0800

    (feat): active nav url state - minor refactoring

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -9,32 +9,24 @@ import {
 import styled from "styled-components";
 
 /* --- UI COMPONENTS --- */
+import { Colors } from "@design-system/colors";
 import { NavWrapper } from "@design-system/index";
 
 /* --- STYLED COMPONENTS --- */
 const NavElement = styled((props) => <Link {...props} />)`
-    color: ${(props) => props.theme.colors.link};
+    display: flex;
+    color: ${(props) => props.active? 
+        `${Colors.sunsetTint}` : props.theme.colors.link };
     text-decoration: ${(props) =>
         props.textDecoration ? props.textDecoration : "none"};
     font-size: 1rem;
     font-weight: 700;
     text-decoration: none;
-    display: flex;
+    margin: 0 0.75rem;
 
     &:hover {
-        text-decoration: none;
-        background-image: linear-gradient(
-            to right,
-            hsl(180, 100%, 70%),
-            hsl(64, 57%, 82%),
-            hsl(0, 100%, 90%)
-        );
-
-        -webkit-background-clip: text;
-        color: transparent;
-        color: #0a192f;
+        color: ${Colors.sunsetTint};
     }
-    margin: 0 0.75rem;
 `;
 
 const HomeImgWrapper = styled.div`
@@ -58,6 +50,7 @@ const Nav = (): React.ReactElement => {
     `);
 
     const cliSVG = data.cli.publicURL;
+    const activeUrl = getActiveUrl();
 
     return (
         <NavWrapper>
@@ -66,13 +59,60 @@ const Nav = (): React.ReactElement => {
                     <HomeImg alt="Home" src={cliSVG} />
                 </HomeImgWrapper>
             </NavElement>
-            <NavElement to={"/tech"}>Tech</NavElement>
-            <NavElement to={"/blog"}>Blog</NavElement>
-            <NavElement to={"/resume"}>Resume</NavElement>
-            <NavElement to={"/credits"}>Credits</NavElement>
-            {/* <NavElement to={"/life-goals"}>Life Goals</NavElement> */}
+
+            {urls.map(url => {
+                    
+                const isUrlActive = activeUrl.redirectUrl === url.redirectUrl;
+                const active = isUrlActive? "active" : "";
+
+                return (
+                    <NavElement 
+                        key={url.title} 
+                        to={url.redirectUrl} 
+                        active={active}
+                    >
+                        {url.title}
+                    </NavElement>
+                );
+            })}
         </NavWrapper>
     );
 };
+
+function getActiveUrl() {
+    const currentUrl = location.pathname;
+    const matchingUrl = urls.filter(url => {
+
+        if (currentUrl.indexOf(url.redirectUrl) === 0) {
+            return true;
+        }
+        return false;
+    });
+
+    return matchingUrl[matchingUrl.length - 1];
+}
+
+const urls = [
+    {
+        title:       "Home",
+        redirectUrl: "/"
+    },
+    {
+        title:       "Tech",
+        redirectUrl: "/tech"
+    },
+    {
+        title:       "Blog",
+        redirectUrl: "/blog"
+    },
+    {
+        title:       "Resume",
+        redirectUrl: "/resume"
+    },
+    {
+        title:       "Credits",
+        redirectUrl: "/credits"
+    }
+];
 
 export default Nav;
