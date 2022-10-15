e80d7c6447b7bb984664baf774405cdc4f060ee7
YeeHan <cyh0011215@gmail.com>
2022-10-16 03:55:15 +0800

    refactor(context/context-site-routes): new

---
+++ src/context/context-site-routes.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export interface I_ContextState {
+    route: string;
+    setRoute: (route: string) => void;
+}
+
+const defaultContextState: I_ContextState = {
+    route:    "",
+    setRoute: () => null
+};
+
+export const SiteRoutesContext = React.createContext(defaultContextState);
+
+export function SiteRoutesProvider(props: {
+    children: React.ReactNode;
+}) {
+    const [ route, setRoute ] = React.useState<string>("");
+
+    return (
+        <SiteRoutesContext.Provider value={{
+            route,
+            setRoute
+        }}>
+            {props.children}
+        </SiteRoutesContext.Provider>
+    );
+}
