32adea8e34fa0f5a10c34a657dd3c2be70648f4c
YeeHan <cyh0011215@gmail.com>
2022-10-15 23:39:41 +0800

    refactor(src/html): crossOrigin to anonymous

--- src/html.js
+++ src/html.js
@@ -7,7 +7,7 @@ export default function HTML(props) {
         <html {...props.htmlAttributes}>
             <head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet" />
 
                 <script
