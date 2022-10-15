d512cf99a17ccba868f02bd0bb5c2b87ffafca65
YeeHan <cyh0011215@gmail.com>
2022-10-15 23:55:30 +0800

    refactor(design/ButtonWrapper): higher font-weight for small size

--- src/design-system/ButtonWrapper.tsx
+++ src/design-system/ButtonWrapper.tsx
@@ -3,24 +3,25 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "gatsby";
 
-const ButtonWrapper = styled((props) => <Link {...props} />)`
-    font-size: ${(props) => {
-        switch (props.size) {
-        case "medium":
-            return "1.5rem";
-        case "small":
-            return "0.9rem";
-        case "xSmall":
-            return "0.8rem";
-        default:
-            return "1.125rem";
-        }
-    }};
+const ButtonWrapper = styled((props): JSX.Element => <Link {...props} />)`
     color: #3fa4e1;
-    font-weight: 500;
     cursor: pointer;
     text-decoration: none;
     line-height: 1.2rem;
+    font-size: ${(props): string => {
+        switch (props.size) {
+        case "medium": return "1.5rem";
+        case "small": return "0.9rem";
+        case "xSmall": return "0.8rem";
+        default: return "1.125rem";
+        }
+    }};
+    font-weight: ${(props): number => {
+        switch (props.size) {
+        case "small": return 600;
+        default: return 500;
+        }
+    }};
 `;
 
 export default ButtonWrapper;
