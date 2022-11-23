119f3fdcd5ca8f83264efb1e4f6e4691ca92533a
Yee Han <cyh0011215@gmail.com>
2022-11-23 23:53:32 +0800

    chore(design/NavWrapper): remove codes

--- src/design-system/NavWrapper.tsx
+++ src/design-system/NavWrapper.tsx
@@ -12,21 +12,4 @@ export const NavWrapper = styled.nav`
     display: flex;
     padding-top: 50px;
     text-align: center;
-
-    // @media ${(props) => props.theme.breakpoints.mobile} {
-    //     display: grid;
-
-    //     padding-top: 3rem;
-    //     margin-left: 0;
-    //     margin-right: auto;
-        
-    //     justify-items: center;
-    //     align-items: center;
-    //     align-content: center;
-
-    //     grid-row: auto auto;
-    //     grid-column-gap: 20px;
-    //     grid-row-gap: 20px;
-    //     grid-template-columns: 25px 100px 25px;
-    // }
 `;
