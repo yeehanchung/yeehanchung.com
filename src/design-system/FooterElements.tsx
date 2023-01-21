01f8e9fb72a50fd0158d4e6925e08df4dd02b00d
YeeHan <cyh0011215@gmail.com>
2023-01-21 23:54:04 +0800

    refactor(design/footerElements): not to align item center anymore

--- src/design-system/FooterElements.tsx
+++ src/design-system/FooterElements.tsx
@@ -2,28 +2,15 @@
 import styled from "styled-components";
 
 const FooterWrapper = styled.div`
-    grid-column: 2 / span 12;
-    min-height: 11.25rem;
     padding-top: 25px;
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-    grid-row: span 2/11;
-
-    @media ${(props) => props.theme.breakpoints.tablet} {
-        grid-column: 2 / span 6;
-    }
 `;
 
 const FooterSocialWrapper = styled.div`
     display: flex;
     flex-wrap: wrap;
-    align-items: center;
-    text-align: center;
     margin-bottom: 100px;
 
     p {
-        text-align: center;
         flex: 0 0 100%;
     }
 `;
